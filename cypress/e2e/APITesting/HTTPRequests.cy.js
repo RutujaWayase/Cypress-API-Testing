155ca752f78a048b0c42f899bda2d5bf5acd5c26
RutujaWayase <rutujawayase1234@gmail.com>
2024-06-28 09:05:41 +0530

    adding httprequests

--- cypress/e2e/APITesting/HTTPRequests.cy.js
+++ cypress/e2e/APITesting/HTTPRequests.cy.js
@@ -2,6 +2,9 @@ describe("HTTP Requests", () => {
 
     it("GET Call", () => {
         cy.request('GET', 'https://jsonplaceholder.tyicode.com/posts/1')
+        .its('status')
+        .should('equal', 200);
+        
     })
 
 })
