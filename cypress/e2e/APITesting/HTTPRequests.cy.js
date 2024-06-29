0182e9bc26689b7a1d0fe772b7e41e2018c71ae0
RutujaWayase <rutujawayase1234@gmail.com>
2024-06-29 11:43:43 +0530

    adding httprequests test

--- cypress/e2e/APITesting/HTTPRequests.cy.js
+++ cypress/e2e/APITesting/HTTPRequests.cy.js
@@ -7,4 +7,46 @@ describe("HTTP Requests", () => {
         
     })
 
+    it("POST Call", () => {
+        cy.request({
+            method: 'POST',
+            url: 'https://jsonplaceholder.tyicode.com/posts/',
+            body: {
+                title: "Test post",
+                body: "This is post call",
+                userID: 1
+            }
+
+        }
+           
+        )
+        .its('status')
+        .should('equal', 201);
+    })
+
+    it("PUT Call", () => {
+        cy.request({
+            method: 'PUT',
+            url: 'https://jsonplaceholder.tyicode.com/posts/1',
+            body: {
+                title: "Test post - Updated",
+                body: "This is put call",
+                userid: 1,
+                id: 1
+
+            }
+        })
+        .its('status')
+        .should('equal', 200);
+    })
+
+    it("DELETE Call", () => {
+        cy.request({
+            method: 'DELETE',
+            url: 'https://jsonplaceholder.tyicode.com/posts/',
+        })
+        .its('status')
+        .should('equal', 200);
+    })
+
 })
