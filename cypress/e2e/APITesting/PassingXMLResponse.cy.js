0ae41f273fb13949c24bfb5851c652e5ed90b78a
RutujaWayase <rutujawayase1234@gmail.com>
2024-06-30 22:42:31 +0530

    adding to passingxmlresoponse test

--- cypress/e2e/APITesting/PassingXMLResponse.cy.js
+++ cypress/e2e/APITesting/PassingXMLResponse.cy.js
@@ -52,7 +52,7 @@ describe("XML Parsing", ()=> {
 
 const xmlPayload = "<Pet>    <id>0</id>    <Category>       <id>0</id>       <name>Dog</name>    </Category>    <name>Jimmy</name>    <photoUrls>       <photoUrl>string</photoUrl>    </photoUrls>    <tags>       <Tag>          <id>0</id>          <name>string</name>       </Tag>    </tags>    <status>available</status> </Pet>"
 let petid = null;
-    it("creating PET",() => {
+    before("creating PET",() => {
         cy.request({
             method: 'POST',
             url: "https://petstore.swagger.io/v2/pet",
@@ -68,6 +68,24 @@ let petid = null;
             parser.parseString(response.body,(error,result)=> {
                 petid = result.Pet.id;
             })
-        })
-        })
+        });
+        });
+
+        it("Fetching Pet data-parsing xml response",() => {
+            cy.request({
+                method: 'GET',
+                url: "https://petstore.swagger.io/v2/pet"+petid,
+                headers: {
+                    "accept": "application/xml"
+                }
+            })
+            .then((response)=> {
+                expect(response.status).to.eq(200);
+                //pass the response using parser
+                parser.parseString(response.body,(error,result)=> {
+                    expect(result.Pet.name).to.equal("Jimmy")
+                    expect(result.Pet.id).to.equal(petid)
+                })
+            });
+            });
 })
